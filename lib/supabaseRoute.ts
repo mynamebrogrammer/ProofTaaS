ca76e1d7fd1b016c732baca8e6469bab0cbfad4e
george hannawi <habibmaksoud@gmail.com>
2026-01-28 09:27:56 -0800

    added connection to supabase database and created lib folder

---
+++ lib/supabaseRoute.ts
@@ -0,0 +1,23 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export function supabaseRouteClient() {
+  const cookieStore = cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        async getAll() {
+          return (await cookieStore).getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(async ({ name, value, options }) => {
+            (await cookieStore).set(name, value, options);
+          });
+        },
+      },
+    }
+  );
+}
